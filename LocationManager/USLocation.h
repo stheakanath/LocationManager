e8bc1503653bf3bf67612f6181e4db15da7d563b
Sony Theakanath <stheakanath@gmail.com>
2013-08-08 11:08:30 -0700

    Adding all files
    Project to compliment Should I Wear Pants Today

---
+++ LocationManager/USLocation.h
@@ -0,0 +1,18 @@
+//
+//  USLocation.h
+//  Wear Pants
+//
+//  Created by Sony Theakanath on 6/18/13.
+//  Copyright (c) 2013 Sony Theakanath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface USLocation : NSObject 
+
++ (NSString*) infofromcity:(NSString*)wantcity;
++ (NSString*) infofromzipcode:(NSString*)wantzipcode;
++ (NSArray*) zipcode;
++ (NSArray*) city;
+@end
--- LocationManager/ViewController.m
+++ LocationManager/ViewController.m
@@ -17,6 +17,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    NSMutableArray *array = [[USLocation city] mutableCopy];
+    NSMutableArray *array2 = [[USLocation zipcode] mutableCopy];
+    
+    NSArray *copy = [[USLocation city] copy];
+    
+    NSInteger index = [copy count] - 1;
+    
+    for (id object in [copy reverseObjectEnumerator]) {
+        
+        if ([array indexOfObject:object inRange:NSMakeRange(0, index)] != NSNotFound) {
+            [array removeObjectAtIndex:index];
+            [array2 removeObjectAtIndex:index];
+        }
+        index--;
+    }
+    
+    NSMutableString *elements = [[NSMutableString alloc] initWithString:@"["];
+    for(int x = 0; x < [array count]; x++) {
+        [elements appendFormat:@"%@@\"%@\"",(x==0?@"":@", "),[array objectAtIndex:x]];
+    }
+    [elements appendString:@"];"];
+        NSLog(@"%@", elements);
+     NSLog(@"\n\n\n\n\nn\n\n\n\n\\n\nn\n\n\n\n\n\n\n\n\n\n\nn\n\n\n\n\n\n");
+    
+     elements = [[NSMutableString alloc] initWithString:@"["];
+    for(int x = 0; x < [array2 count]; x++) {
+        [elements appendFormat:@"%@@\"%@\"",(x==0?@"":@", "),[array2 objectAtIndex:x]];
+    }
+    [elements appendString:@"];"];
+    NSLog(@"%@", elements);
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
